7cb18df87f46615a9982b4dc85e1a149edb7ff2f
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    more smoke tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 using LazyGrids
+using OnlineStatsBase
 using MultivariateECDFs
+
 
 @testset begin
     n = 100
@@ -8,6 +10,13 @@ using MultivariateECDFs
     data_nt = NamedTuple{(:a, :b)}.(data_t)
     
     ecdf_evaluate(data_t, grid(-1:0.2:1, -1:0.2:2))
+    ecdf_evaluate(data_t, grid(-1:0.2:1, -1:0.2:2), aggregate=(Orders.Above(), Orders.Below()))
+    ecdf_evaluate(data_nt, grid(a=-1:0.2:1, b=-1:0.2:2))
+    ecdf_evaluate(data_nt, grid(a=-1:0.2:1, b=-1:0.2:2), aggregate=(a=Orders.Above(),))
+    ecdf_evaluate(data_nt, grid(b=-1:0.2:2, a=-1:0.2:1), aggregate=(a=Orders.Above(),))
+    ecdf_evaluate(Counter(NamedTuple), data_nt, grid(a=-1:0.2:1, b=-1:0.2:2), aggregate=(a=Orders.Below(), b=Orders.Below()))
+    ecdf_evaluate(FTSeries(NamedTuple, Counter(), Variance(), Mean(), transform=x -> x.a), data_nt, grid(a=-1:0.2:1, b=-1:0.2:2), aggregate=(a=Orders.NoAggBelow(),)) .|> value
+    @test ecdf_evaluate(data_t, grid(-1:0.2:1, -1:0.2:2), aggregate=(Orders.Below(), Orders.Below())) == ecdf_evaluate(data_t, grid(-1:0.2:1, -1:0.2:2))
 end
 
 
