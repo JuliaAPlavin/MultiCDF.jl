4bfff1b18be76a780cce16ae2e5f511b7ac445a4
aplavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    Deploying to gh-pages from  @ 0f06fa52550c5467b23f981d8a13ebc530a003e2 🚀

---
+++ src/MultiCDFs.jl
@@ -0,0 +1,104 @@
+module MultiCDFs
+
+export ecdf
+
+using RectiGrids
+using OnlineStatsBase: OnlineStat, fit!, merge, value
+using AxisKeys: hasnames
+
+
+struct ECDF{T, TD <: AbstractVector{T}, TS}
+	data::TD
+	signs::TS
+end
+
+default_signs(::Type{<:NamedTuple{NS}}) where {NS} = NamedTuple{NS}(ntuple(_ -> <=, length(NS)))
+default_signs(::Type{T}) where {T <: Tuple} = ntuple(_ -> <=, fieldcount(T))
+default_signs(::Type{T}) where {T <: AbstractVector} = ntuple(_ -> <=, length(T))  # length(T) only works for StaticArrays, that's fine
+default_signs(::Type{<:Real}) = <=
+
+ecdf(data; signs=default_signs(eltype(data))) = ECDF(data, signs)
+
+function (ecdf::ECDF)(x)
+	count(y -> is_all_leq(ecdf.signs, y, x), ecdf.data) / length(ecdf.data)
+end
+
+function (ecdf::ECDF)(x, ::typeof(count))
+	count(y -> is_all_leq(ecdf.signs, y, x), ecdf.data)
+end
+
+function (ecdf::ECDF)(x, stat::OnlineStat)
+	fit!(copy(stat), filter(y -> is_all_leq(ecdf.signs, y, x), ecdf.data))
+end
+
+Broadcast.broadcasted(ecdf::ECDF, g::RectiGrid) = ecdf.(g, count) ./ length(ecdf.data)
+
+function Broadcast.broadcasted(ecdf::ECDF, g::RectiGrid, ::typeof(count))
+	axkeys = hasnames(g) ? named_axiskeys(g) : axiskeys(g)
+	axspecs = map(axkeys, select_if_possible(ecdf.signs, axkeys)) do ax, sign
+		@assert issorted(ax)
+		ECDFAxisSpec(ax, sign)
+	end
+	counts = map(_ -> 0, g)
+	for r in ecdf.data
+		ix = findbin(axspecs, r) |> CartesianIndex
+		checkbounds(Bool, counts, ix) || continue
+		counts[ix] += 1
+	end
+	for (dim, ax) in enumerate(axspecs)
+		aggregate_axis!(ax.sign, counts, dim)
+	end
+	return counts
+end
+
+function Broadcast.broadcasted(ecdf::ECDF, g::RectiGrid, stat::OnlineStat)
+	axkeys = hasnames(g) ? named_axiskeys(g) : axiskeys(g)
+	axspecs = map(axkeys, select_if_possible(ecdf.signs, axkeys)) do ax, sign
+		@assert issorted(ax)
+		ECDFAxisSpec(ax, sign)
+	end
+	result = map(_ -> copy(stat), g)
+	for r in ecdf.data
+		ix = findbin(axspecs, r) |> CartesianIndex
+		checkbounds(Bool, result, ix) || continue
+		fit!(result[ix], r)
+	end
+	for (dim, ax) in enumerate(axspecs)
+		aggregate_axis!(ax.sign, result, dim)
+	end
+	return result
+end
+
+# like Base.AbstractVecOrTuple, but includes heterogeneous tuples
+const AbstractVecOrTuple = Union{AbstractVector, Tuple}
+
+@inline is_all_leq(sign, datap::Real, query::Real) = sign(datap, query)
+@inline is_all_leq(sign, datap::Real, query::Base.Fix2) = query(datap)  # ignore `sign`
+@inline is_all_leq(signs, datap, query) = all(map(is_all_leq, select_if_possible(signs, query), select_if_possible(datap, query), query))
+
+# can specify a subset of fields for namedtuples...
+select_if_possible(data::NamedTuple, query::NamedTuple{NSQ}) where {NSQ} = data[NSQ]
+# but not for regular tuples
+select_if_possible(data::AbstractVecOrTuple, query::AbstractVecOrTuple) = (@assert length(data) == length(query); data)
+
+struct ECDFAxisSpec{TV, TO}
+    binedges::TV
+    sign::TO
+end
+
+@inline findbin(a::ECDFAxisSpec, x) = findbin(a.sign, a.binedges, x)
+
+@inline findbin(as::Tuple, xs::Tuple) = map((ax, x) -> findbin(ax, x), as, xs)
+@inline findbin(as::Tuple, xs::AbstractVector) = findbin(as, Tuple(xs))
+@inline findbin(as::NamedTuple{NS}, xs::NamedTuple) where {NS} = map((ax, x) -> findbin(ax, x), as, xs[NS]) |> values
+
+@inline findbin(::typeof(<=), binedges::AbstractVector, x) = searchsortedfirst(binedges, x)
+@inline findbin(::typeof(>=), binedges::AbstractVector, x) = searchsortedlast(binedges, x)
+
+# aggregate_axis!(::Union{NoAggBelow, NoAggAbove}, A::AbstractArray, dim::Int) = A
+aggregate_axis!(::typeof(<=), A::AbstractArray{<:Number}, dim::Int) = cumsum!(A, A, dims=dim)
+aggregate_axis!(::typeof(<=), A::AbstractArray, dim::Int) = accumulate!(merge, A, A, dims=dim)
+aggregate_axis!(::typeof(>=), A::AbstractArray, dim::Int) = (reverse!(A, dims=dim); cumsum!(A, A, dims=dim); reverse!(A, dims=dim))
+
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,122 @@
+using Base: NamedTuple
+import Random
+using Test
+using RectiGrids
+using OnlineStatsBase
+using StableRNGs
+using StaticArrays
+import StatsBase
+using Accessors
+using MultiCDFs
+
+
+@testset "empty" begin
+    m_ecdf = ecdf(NamedTuple{(:a,)}[])
+    @test m_ecdf((a=1,)) |> isnan
+    @test m_ecdf((a=1,), count) == 0
+    @test m_ecdf((a=1,), Counter(NamedTuple)) |> value == 0
+end
+
+@testset "numbers" begin
+    rng = StableRNG(123)
+    data = randn(rng, 100)
+    m_ecdf = ecdf(data)
+    sb_ecdf = StatsBase.ecdf(data)
+
+    @test m_ecdf.(-2:0.5:2) ≈ sb_ecdf(-2:0.5:2)
+    @test m_ecdf.(data) ≈ sb_ecdf(data)
+    xs = randn(rng, 10)
+    @test m_ecdf.(xs) ≈ sb_ecdf(xs) ≈ [0.24, 0.28, 0.01, 0.63, 0.02, 0.01, 0.54, 0.73, 0.02, 0.7]
+    @test m_ecdf(-Inf) == 0
+    @test m_ecdf(Inf) == 1
+
+    @test_throws MethodError m_ecdf((0.,))
+    @test_throws MethodError m_ecdf(SVector(0.,))
+    
+    @test m_ecdf(<=(-Inf)) == 0
+    @test m_ecdf(>=(-Inf)) == 1
+    @test m_ecdf(0) == m_ecdf(<=(0)) == 1 - m_ecdf(>=(0)) == 0.47
+    @test m_ecdf(0, count) == m_ecdf(<=(0), count) == 100 - m_ecdf(>=(0), count) == 47
+end
+
+@testset for T in [Tuple, SVector]
+    rng = StableRNG(123)
+    data = T.(Tuple.(eachrow(randn(rng, 100, 2))))
+
+    sb_ecdf1 = StatsBase.ecdf(getindex.(data, 1))
+    m_ecdf = ecdf(data)
+
+    @test m_ecdf.(tuple.(-2:0.5:2, Inf)) ≈ sb_ecdf1(-2:0.5:2) ≈ [0.01, 0.02, 0.13, 0.29, 0.47, 0.6, 0.81, 0.89, 0.95]
+    @test m_ecdf((-Inf, -Inf)) == m_ecdf((0., -Inf)) == m_ecdf((Inf, -Inf)) == m_ecdf((-Inf, 0.)) == m_ecdf((-Inf, Inf)) == 0
+    @test m_ecdf((Inf, Inf)) == 1
+    @test m_ecdf((0, 1)) == m_ecdf(T((0, 1))) == m_ecdf((<=(0), <=(1))) == 0.38
+    @test m_ecdf(T((0, >=(1))), count) == m_ecdf((<=(0), >=(1)), count) == 9
+
+    @test_throws Exception m_ecdf(())
+    @test_throws Exception m_ecdf((a=1, b=2))
+
+    e1 = m_ecdf.(grid(T, -1:0.5:1, -1:1:2), count)
+    @test e1 == [2 6 9 13; 4 12 22 28; 7 21 38 46; 9 27 50 58; 13 40 70 79]
+    @test e1 == m_ecdf.(grid(T, -1:0.5:1, -1:1:2) |> collect, count)
+    @test e1[1, 3] == 9
+    @test e1(0, -1) == 7
+
+    e2 = (@set m_ecdf.signs[1] = >=).(grid(T, -1:0.5:1, -1:1:2))
+    @test e2 == [15 47 78 85; 13 41 65 70; 10 32 49 52; 8 26 37 40; 4 13 17 19] ./ 100
+    @test e2 == (@set m_ecdf.signs[1] = >=).(grid(T, -1:0.5:1, -1:1:2) |> collect)
+end
+
+@testset "namedtuples" begin
+    rng = StableRNG(123)
+    data = NamedTuple{(:a, :b)}.(eachrow(randn(rng, 100, 2)))
+    sb_ecdf1 = StatsBase.ecdf(getindex.(data, 1))
+
+    m_ecdf = ecdf(data)
+    @test m_ecdf((a=-Inf, b=-Inf)) == m_ecdf((a=0., b=-Inf)) == m_ecdf((a=Inf, b=-Inf)) == m_ecdf((a=-Inf, b=0.)) == m_ecdf((a=-Inf, b=Inf)) == 0
+    @test m_ecdf((a=Inf, b=Inf)) == 1
+    @test m_ecdf((b=1, a=0)) == m_ecdf((a=0, b=1))
+    @test m_ecdf((b=1,)) == 0.87
+    @test m_ecdf((b= >=(1),), count) == 13
+    @test m_ecdf.(NamedTuple{(:a, :b)}.(tuple.(-2:0.5:2, Inf))) ≈ sb_ecdf1(-2:0.5:2)
+    @test m_ecdf.(NamedTuple{(:a,)}.(-2:0.5:2)) ≈ sb_ecdf1(-2:0.5:2)
+    @test_throws ErrorException m_ecdf((a=0., c=0.))
+    
+    @test m_ecdf((a= <=(-Inf), b= <=(-Inf))) == 0
+    @test m_ecdf((a= >=(-Inf), b= >=(-Inf)), count) == 100
+    @test m_ecdf((a=0, b=0)) +
+            m_ecdf((a= >=(0), b= >=(0))) +
+            m_ecdf((a= >=(0), b=0)) +
+            m_ecdf((a=0, b= >=(0))) == 1
+
+    e3 = m_ecdf.(grid(a=-1:0.5:1, b=-1:1:2), count)
+    @test e3[1, 3] == 9
+    @test e3(a=0, b=-1) == 7
+    @test e3(b=-1, a=0) == 7
+
+    e4 = (@set m_ecdf.signs.a = >=).(grid(a=-1:0.5:1, b=-1:1:2))
+    @test e4[1, 1] == m_ecdf((a= >=(-1), b=-1))
+    @test e4(-0.5, 0) == m_ecdf((a= >=(-0.5), b=0))
+    @test e4(a=-0.5, b=0) == m_ecdf((a= >=(-0.5), b=0))
+
+    e5 = (@set m_ecdf.signs.a = >=).(grid(b=-1:1:2, a=-1:0.5:1), count)
+    @test e5 == permutedims(e4) .* 100
+
+    @test m_ecdf.(grid(a=-1:0.5:1, b=-1:1:2)) == m_ecdf.(grid(a=-1:0.5:1, b=-1:1:2) |> collect, count) ./ 100
+
+    e6 = m_ecdf.(grid(a=-1:0.5:1, b=-1:1:2), Counter(NamedTuple))
+    @test value.(e6) == e3
+
+    e7 = m_ecdf.(grid(a=-1:0.5:1))
+    @test e7(a=0.5) == m_ecdf((a=0.5, b=Inf)) == 0.6
+end
+
+
+import Aqua
+import CompatHelperLocal as CHL
+@testset begin
+    CHL.@check()
+    Aqua.test_ambiguities(MultiCDFs, recursive=false)
+    Aqua.test_unbound_args(MultiCDFs)
+    Aqua.test_undefined_exports(MultiCDFs)
+    Aqua.test_stale_deps(MultiCDFs)
+end
